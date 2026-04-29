61b00a68f7b59150da5f0f9824d33a854bb4d6e5
Griffin Hines <griffinhines@comcast.net>
2026-04-29 18:47:45 -0400

    Update page.tsx

--- griffin-vending/src/app/page.tsx
+++ griffin-vending/src/app/page.tsx
@@ -1,5 +1,5 @@
+import GriffinVendingSite from "@/components/GriffinVendingSite";
+
 export default function Page() {
-  // If you put the file in src/components/GriffinVendingSite.tsx:
-  const GriffinVendingSite = require("@/components/GriffinVendingSite").default;
   return <GriffinVendingSite />;
 }
